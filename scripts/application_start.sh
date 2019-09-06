fdccbd771db1f84c81a564a59309a4c8b9dca844
Glenn Meyer <glennmeyer@gmail.com>
2019-09-06 15:26:46 -0500

    fix application_start.sh

--- scripts/application_start.sh
+++ scripts/application_start.sh
@@ -3,4 +3,4 @@
 docker stack rm docker-tutorial
 docker swarm leave --force
 docker swarm init
-docker stack deploy -c docker-compose.yml docker-tutorial
+docker stack deploy -c /docker-tutorial/docker-compose.yml docker-tutorial
